5740c6e219563ea2e1f85e479c990359a6b7204a
s121528 <s121528@163.com>
2017-11-22 17:42:18 +0800

    Java配置
    Java配置

---
+++ learning-01-02/src/main/java/cn/hacz/edu/learning0102/Learning0102Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu.learning0102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning0102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning0102Application.class, args);
+	}
+}
