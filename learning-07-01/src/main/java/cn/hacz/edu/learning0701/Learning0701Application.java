b70acfe1e35711d747ea5534b751a97ac6be6ada
s121528 <s121528@163.com>
2017-11-22 15:19:44 +0800

    learning-07-01
    learning-07-01

---
+++ learning-07-01/src/main/java/cn/hacz/edu/learning0701/Learning0701Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu.learning0701;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning0701Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning0701Application.class, args);
+	}
+}
