b0979c9ba99331fb3a3c8afeb641d6e80cff5cb8
s121528 <s121528@163.com>
2017-11-22 22:09:31 +0800

    SpringBoot添加Java8的时间日期的Jackson2支持
    SpringBoot添加Java8的时间日期的Jackson2支持

---
+++ Leaning-11-01/src/main/java/cn/hacz/edu/Leaning1101Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Leaning1101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Leaning1101Application.class, args);
+	}
+}
