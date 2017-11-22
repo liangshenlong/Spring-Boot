292e6f4a97d12c81ff619b2dd9ae2f17b90e28cf
s121528 <s121528@163.com>
2017-11-22 13:46:49 +0800

    JPA
    JPA

--- Learning-03-01/src/main/java/cn/hacz/edu/controller/UserController.java
+++ Learning-03-01/src/main/java/cn/hacz/edu/controller/UserController.java
@@ -1,7 +1,6 @@
 package cn.hacz.edu.controller;
 
 import cn.hacz.edu.dao.UserDaoI;
-import cn.hacz.edu.entity.UserEntity;
 import cn.hacz.edu.webexception.BaseController;
 import cn.hacz.edu.webexception.R;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 public class UserController extends BaseController {
