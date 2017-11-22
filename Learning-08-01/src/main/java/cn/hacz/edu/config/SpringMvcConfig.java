7aeb46c02919fa15c47a30be645331f004f24cce
s121528 <s121528@163.com>
2017-11-22 15:57:25 +0800

    SpringMvcConfig
    SpringMvcConfig

--- Learning-08-01/src/main/java/cn/hacz/edu/config/SpringMvcConfig.java
+++ Learning-08-01/src/main/java/cn/hacz/edu/config/SpringMvcConfig.java
@@ -1,8 +1,10 @@
 package cn.hacz.edu.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 public class SpringMvcConfig extends WebMvcConfigurerAdapter {
     //自定义静态资源文件路径
     @Override
