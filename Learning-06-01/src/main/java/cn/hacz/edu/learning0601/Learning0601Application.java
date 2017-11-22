c7b8192da3d4249d7bd1babf566fd22fef9ca0a4
s121528 <s121528@163.com>
2017-11-22 14:34:56 +0800

    filter
    filter

---
+++ Learning-06-01/src/main/java/cn/hacz/edu/learning0601/Learning0601Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu.learning0601;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning0601Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning0601Application.class, args);
+	}
+}
---
+++ Learning-06-01/src/test/java/cn/hacz/edu/learning0601/Learning0601ApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.hacz.edu.learning0601;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Learning0601ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
