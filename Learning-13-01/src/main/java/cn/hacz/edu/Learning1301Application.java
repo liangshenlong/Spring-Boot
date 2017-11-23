12b8ed91e3a96c902ab8470361470281a5209e7c
s121528 <s121528@163.com>
2017-11-23 20:12:04 +0800

    Swagger接口文档
    Swagger接口文档

---
+++ Learning-13-01/src/main/java/cn/hacz/edu/Learning1301Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning1301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning1301Application.class, args);
+	}
+}
