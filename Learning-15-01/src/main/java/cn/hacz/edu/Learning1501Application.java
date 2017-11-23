2708de11010e928505f529bee51181150099e918
s121528 <s121528@163.com>
2017-11-23 20:15:19 +0800

    程序监控（admin）
    程序监控（admin）

---
+++ Learning-15-01/src/main/java/cn/hacz/edu/Learning1501Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning1501Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning1501Application.class, args);
+	}
+}
