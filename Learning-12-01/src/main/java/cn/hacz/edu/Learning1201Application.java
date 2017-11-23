90cf676d87e816415bbeaaf5ff7ecc4cb4c5bff0
s121528 <s121528@163.com>
2017-11-23 09:20:17 +0800

    HTTPclient接口、构建RESTful API+HTTPclient接口
    HTTPclient接口、构建RESTful API+HTTPclient接口

---
+++ Learning-12-01/src/main/java/cn/hacz/edu/Learning1201Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning1201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning1201Application.class, args);
+	}
+}
