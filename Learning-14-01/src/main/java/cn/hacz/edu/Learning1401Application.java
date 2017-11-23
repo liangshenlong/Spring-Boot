602913b4a615d00ef6a084df7703b3bbb0516221
s121528 <s121528@163.com>
2017-11-23 20:13:51 +0800

    模板视图引擎
    模板视图引擎

---
+++ Learning-14-01/src/main/java/cn/hacz/edu/Learning1401Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning1401Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning1401Application.class, args);
+	}
+}
