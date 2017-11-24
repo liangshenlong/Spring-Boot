2dd57bdf408c23b6e7f51791769cb02c7b9bc456
s121528 <s121528@163.com>
2017-11-24 13:49:09 +0800

    RestTemplate接口调用
    RestTemplate接口调用

---
+++ Learning-12-04/src/main/java/cn/hacz/edu/Learning1204Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning1204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning1204Application.class, args);
+	}
+}
