82db99bfb5a4dc46f2b6639841b80112f0960b33
s121528 <s121528@163.com>
2017-11-26 11:48:22 +0800

    配置使用FastJson返回Json视图、Json的序列化和反序列化
    配置使用FastJson返回Json视图、Json的序列化和反序列化

--- Learning-05-01/src/main/java/cn/hacz/edu/Learning0501Application.java
+++ Learning-05-01/src/main/java/cn/hacz/edu/Learning0501Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Learning0301Application {
+public class Learning0501Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Learning0301Application.class, args);
+		SpringApplication.run(Learning0501Application.class, args);
 	}
 }
