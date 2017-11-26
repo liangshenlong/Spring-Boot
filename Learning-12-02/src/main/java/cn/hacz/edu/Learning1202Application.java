79d6ba280a1b53466de05fff37569a0b699af459
s121528 <s121528@163.com>
2017-11-26 11:50:41 +0800

    Okhttp接口调用
    Okhttp接口调用

---
+++ Learning-12-02/src/main/java/cn/hacz/edu/Learning1202Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning1202Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning1202Application.class, args);
+	}
+}
