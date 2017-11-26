5fa646fad61b2fcd8989457b4e1562c8a6d2e201
s121528 <s121528@163.com>
2017-11-26 11:52:28 +0800

    Httpurlconnection接口调用
    Httpurlconnection接口调用

---
+++ Learning-12-03/src/main/java/cn/hacz/edu/Learning1203Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning1203Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning1203Application.class, args);
+	}
+}
