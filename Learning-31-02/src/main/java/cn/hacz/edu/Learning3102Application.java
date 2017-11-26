eb3cafdd6e39a0d043e152bab73670d83118e212
s121528 <s121528@163.com>
2017-11-26 15:17:50 +0800

    hibernate
    hibernate

---
+++ Learning-31-02/src/main/java/cn/hacz/edu/Learning3102Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning3102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning3102Application.class, args);
+	}
+}
