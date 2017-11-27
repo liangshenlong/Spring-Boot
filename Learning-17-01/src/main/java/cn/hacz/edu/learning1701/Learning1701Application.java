f12e31d66b2c818691656e822561f81a17767686
s121528 <s121528@163.com>
2017-11-27 10:11:57 +0800

    SpringBoot使用Lombok来优雅的编码
    SpringBoot使用Lombok来优雅的编码

---
+++ Learning-17-01/src/main/java/cn/hacz/edu/learning1701/Learning1701Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu.learning1701;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning1701Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning1701Application.class, args);
+	}
+}
---
+++ Learning-17-01/src/test/java/cn/hacz/edu/learning1701/Learning1701ApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.hacz.edu.learning1701;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Learning1701ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
