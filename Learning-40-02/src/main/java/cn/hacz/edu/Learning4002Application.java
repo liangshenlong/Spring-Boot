8b359133acdab2fea9cf25b850be9ff51e905663
s121528 <s121528@163.com>
2017-11-27 12:54:56 +0800

    SpringSecurity让SpringBoot项目更安全，实现登录
    	SpringSecurity让SpringBoot项目更安全，实现登录

--- Learning-40-02/src/main/java/cn/hacz/edu/Learning4002Application.java
+++ Learning-40-02/src/main/java/cn/hacz/edu/Learning4002Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Learning4002Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Learning4002Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Learning4002Application.class, args);
+    }
 }
