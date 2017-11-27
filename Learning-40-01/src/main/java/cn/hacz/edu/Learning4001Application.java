b7670e8a814e095a342ae30c43995b7bc3e7ddb0
s121528 <s121528@163.com>
2017-11-27 11:32:37 +0800

    使用SpringSecurity让SpringBoot项目更安全
    使用SpringSecurity让SpringBoot项目更安全

---
+++ Learning-40-01/src/main/java/cn/hacz/edu/Learning4001Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning4001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning4001Application.class, args);
+	}
+}
--- Learning-40-02/src/main/java/cn/hacz/edu/Learning4002Application.java
+++ Learning-40-02/src/main/java/cn/hacz/edu/Learning4002Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Learning4001使用SpringSecurity让SpringBoot项目更安全Application {
+public class Learning4002Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Learning4001使用SpringSecurity让SpringBoot项目更安全Application.class, args);
+		SpringApplication.run(Learning4002Application.class, args);
 	}
 }
