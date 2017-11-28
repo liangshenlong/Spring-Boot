04cb1dbe4bcbb07578ec45eb53670d1b5334b6c7
s121528 <s121528@163.com>
2017-11-28 11:42:06 +0800

    Velocity 模板引擎介绍
    Velocity 模板引擎介绍

---
+++ Learning-50-01/src/main/java/cn/hacz/edu/Learning5001Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning5001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning5001Application.class, args);
+	}
+}
