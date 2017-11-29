01770bf24f2cb9c4c01d30d3e87441da138a1b99
s121528 <s121528@163.com>
2017-11-29 09:23:14 +0800

    WEB应用开发-模板引擎FreeMarker
    WEB应用开发-模板引擎FreeMarker

--- Learning-14-01/src/main/java/cn/hacz/edu/Learning1401Application.java
+++ Learning-14-01/src/main/java/cn/hacz/edu/Learning1401Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Learning1401Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Learning1401Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Learning1401Application.class, args);
+    }
 }
