003b4c0fd07dbcb494c9d75864e3fb415f5c95d3
s121528 <s121528@163.com>
2017-11-29 11:29:41 +0800

    Spring Boot 添加支持CORS跨域访问
    Spring Boot 添加支持CORS跨域访问

---
+++ Learning-18-02/src/main/java/cn/hacz/edu/Learning1802Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning1802Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning1802Application.class, args);
+	}
+}
