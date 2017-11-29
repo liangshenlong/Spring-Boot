696a1317d75574075c6290325960f72dc89512a6
s121528 <s121528@163.com>
2017-11-29 10:12:42 +0800

    WEB应用开发-模板引擎Thymeleaf
    WEB应用开发-模板引擎Thymeleaf

---
+++ Learning-14-02/src/main/java/cn/hacz/edu/Learning1402Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning1402Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning1402Application.class, args);
+	}
+}
