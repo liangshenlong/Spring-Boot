94d8e17d0eac5ed802f4f90bf8e3323601be2df4
s121528 <s121528@163.com>
2017-11-29 16:53:50 +0800

    Annotation实战练习
    Annotation实战练习

---
+++ Learning-01-05/src/main/java/cn/hacz/edu/Learning0105Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning0105Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning0105Application.class, args);
+	}
+}
