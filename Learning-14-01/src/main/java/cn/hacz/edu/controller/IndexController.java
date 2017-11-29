e1d36e81ba1ea3bf85ab8609d01f867c68a09e87
s121528 <s121528@163.com>
2017-11-29 10:08:43 +0800

    WEB应用开发-模板引擎FreeMarker
    WEB应用开发-模板引擎FreeMarker

---
+++ Learning-14-01/src/main/java/cn/hacz/edu/controller/IndexController.java
@@ -0,0 +1,32 @@
+package cn.hacz.edu.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * ========================
+ * Created with IntelliJ IDEA.
+ * User：guod
+ * Date：2017/11/29
+ * Time：9:36
+ * Description：功能模块：
+ * JDK：V1.8
+ * GitHub地址：https://github.com/s121528
+ * ========================
+ */
+@Controller
+public class IndexController {
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping("index")
+    public String index(ModelMap map) {
+        logger.info("这里是controller");
+        map.put("title", "hello world");
+        return "index"; // 注意，不要在最前面加上/，linux下面会出错
+    }
+
+}
