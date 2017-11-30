49c3a3afac69f55e56031c0da1e50e7c73453088
s121528 <s121528@163.com>
2017-11-30 08:33:37 +0800

    反射机制
    反射机制

--- Learning-01-06/src/main/java/cn/hacz/edu/main/ClassMain03.java
+++ Learning-01-06/src/main/java/cn/hacz/edu/main/ClassMain03.java
@@ -6,7 +6,7 @@ package cn.hacz.edu.main;
  * User：Guod
  * Date：2017/11/29
  * Time：20:26
- * Description：功能模块：取得类所在的包
+ * Description：功能模块：通过Java反射机制得到类的包名和类名
  * JDK：V1.8
  * GitHub地址：https://github.com/s121528
  * ========================
@@ -14,6 +14,7 @@ package cn.hacz.edu.main;
 public class ClassMain03 {
     public static void main(String[] args) throws Exception {
         Class<?> s = Class.forName("cn.hacz.edu.entity.Student");
-        System.out.println(s.getPackage().getName());
+        System.out.println("完整类名：" + s.getPackage().getName());
+        System.out.printf("完整类名：" + s.getClass().getName());
     }
 }
