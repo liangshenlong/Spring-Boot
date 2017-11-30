01b80ce43d7f57055c85f79fb6d2b1f42d5019d1
s121528 <s121528@163.com>
2017-11-30 08:35:44 +0800

    项目整合JPA多数据源配置
    项目整合JPA多数据源配置

---
+++ Learning-37-01/src/main/java/cn/hacz/edu/Learning3701Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning3701Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning3701Application.class, args);
+	}
+}
