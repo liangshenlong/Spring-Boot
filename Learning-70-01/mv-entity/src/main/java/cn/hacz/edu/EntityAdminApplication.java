e46868342932e0355a8627115358d9b99cd1611f
s121528 <s121528@163.com>
2017-12-01 14:42:39 +0800

    SpringBoot项目多模块运用与设计SpringBoot项目多模块运用与设计
    SpringBoot项目多模块运用与设计

---
+++ Learning-70-01/mv-entity/src/main/java/cn/hacz/edu/EntityAdminApplication.java
@@ -0,0 +1,22 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ========================
+ * Created with IntelliJ IDEA.
+ * User：guod
+ * Date：2017/12/1
+ * Time：14:19
+ * Description：功能模块：
+ * JDK：V1.8
+ * GitHub地址：https://github.com/s121528
+ * ========================
+ */
+@SpringBootApplication
+public class EntityAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EntityAdminApplication.class, args);
+    }
+}
---
+++ Learning-70-01/mv-entity/src/main/java/cn/hacz/edu/entity/UserEntity.java
@@ -0,0 +1,20 @@
+package cn.hacz.edu.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * ========================
+ * Created with IntelliJ IDEA.
+ * User：guod
+ * Date：2017/12/1
+ * Time：14:26
+ * Description：功能模块：
+ * JDK：V1.8
+ * GitHub地址：https://github.com/s121528
+ * ========================
+ */
+@Entity
+@Table(name = "user_entity")
+public class UserEntity {
+}
