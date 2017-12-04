75ecdc2f851b59797db55e3cba6446c6cf45d7fd
s121528 <s121528@163.com>
2017-12-04 14:16:37 +0800

    Spring Boot 整合mybatis
    Spring Boot 整合mybatis

---
+++ Learning-34-02/src/main/java/cn/hacz/edu/Learning3402Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning3402Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning3402Application.class, args);
+	}
+}
