07b78866d71b8123e1cd56305a0f3f80c3e59376
s121528 <s121528@163.com>
2017-12-04 14:52:42 +0800

    spring boot整合mybatis
    spring boot整合mybatis

---
+++ Learning-34-02/src/main/java/cn/hacz/edu/service/UserServiceI.java
@@ -0,0 +1,20 @@
+package cn.hacz.edu.service;
+
+import cn.hacz.edu.model.User;
+
+import java.util.List;
+
+/**
+ * ========================
+ * Created with IntelliJ IDEA.
+ * User：guod
+ * Date：2017/12/4
+ * Time：14:47
+ * Description：功能模块：
+ * JDK：V1.8
+ * GitHub地址：https://github.com/s121528
+ * ========================
+ */
+public interface UserServiceI {
+    List<User> getUser();
+}
---
+++ Learning-34-02/src/main/java/cn/hacz/edu/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package cn.hacz.edu.service.impl;
+
+import cn.hacz.edu.mapper.UserMapper;
+import cn.hacz.edu.model.User;
+import cn.hacz.edu.service.UserServiceI;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * ========================
+ * Created with IntelliJ IDEA.
+ * User：guod
+ * Date：2017/12/4
+ * Time：14:47
+ * Description：功能模块：
+ * JDK：V1.8
+ * GitHub地址：https://github.com/s121528
+ * ========================
+ */
+public class UserServiceImpl implements UserServiceI {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public List<User> getUser() {
+        return null;
+    }
+}
