11988cd97c980020910442bccdbe0e15f3a0137a
s121528 <s121528@163.com>
2017-12-06 15:05:44 +0800

    实现SpringBoot单个、多个文件的上传
    实现SpringBoot单个、多个文件的上传

--- Learning-09-01/src/main/java/cn/hacz/edu/controller/Upload02Controller.java
+++ Learning-09-01/src/main/java/cn/hacz/edu/controller/Upload02Controller.java
@@ -3,10 +3,15 @@ package cn.hacz.edu.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.Iterator;
 
 /**
  * ========================
@@ -22,8 +27,32 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Controller
 public class Upload02Controller {
+
     @RequestMapping(value = "/upload01")
     public String upload01(@RequestParam("image_icon") CommonsMultipartFile file, HttpServletRequest request, HttpServletResponse response) {
+        return "";
+    }
+
+    /**
+     * 传统的原生处理方式
+     *
+     * @param request
+     * @param response
+     * @return
+     */
+    public String upload02(HttpServletRequest request, HttpServletResponse response) {
+        CommonsMultipartResolver cmr = new CommonsMultipartResolver(request.getSession().getServletContext());
+        if (cmr.isMultipart(request)) {
+            MultipartHttpServletRequest mRequest = (MultipartHttpServletRequest) request;
+            Iterator files = mRequest.getFileNames();
+            String result = "";
+            while (files.hasNext()) {
+                MultipartFile mFile = mRequest.getFile((String) files.next());
+                if (mFile != null) {
+                    String fileName = mFile.getOriginalFilename();
+                }
+            }
+        }
         return "";
     }
 }
