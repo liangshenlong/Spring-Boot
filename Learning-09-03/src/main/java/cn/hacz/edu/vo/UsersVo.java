ee367c262c1bb9cac8cac7634c3544e854824126
s121528 <s121528@163.com>
2017-12-06 15:57:49 +0800

    Spring Boot 数据提交-同步提交方式+异步提交方式
    Spring Boot 数据提交-同步提交方式+异步提交方式

---
+++ Learning-09-03/src/main/java/cn/hacz/edu/vo/UsersVo.java
@@ -0,0 +1,17 @@
+package cn.hacz.edu.vo;
+
+import java.util.List;
+
+public class UsersVo {
+
+	private List<User> users;
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+}
