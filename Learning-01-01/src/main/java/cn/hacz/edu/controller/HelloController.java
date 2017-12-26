8d4426cd9a14ac53c6b243ecb4c2f86299239d7c
s121528 <s121528@163.com>
2017-12-26 22:23:28 +0800

    Spring Boot Hello world
    Spring Boot Hello world

--- Learning-01-01/src/main/java/cn/hacz/edu/controller/HelloController.java
+++ Learning-01-01/src/main/java/cn/hacz/edu/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "/index")
     public String index() {
         return "hello world!";
     }
