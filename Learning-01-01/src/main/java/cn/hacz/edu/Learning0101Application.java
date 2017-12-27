41501614863d4bfd48b646f217a7d3c02f433af6
s121528 <s121528@163.com>
2017-12-27 08:25:38 +0800

    Spring Boot入门之hello world，初探Spring Boot的魅力！
    Spring Boot入门之hello world，初探Spring Boot的魅力！

--- Learning-01-01/src/main/java/cn/hacz/edu/Learning0101Application.java
+++ Learning-01-01/src/main/java/cn/hacz/edu/Learning0101Application.java
@@ -3,10 +3,15 @@ package cn.hacz.edu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 功能：程序入口
+ *
+ * @author guod
+ */
 @SpringBootApplication
 public class Learning0101Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Learning0101Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Learning0101Application.class, args);
+    }
 }
