88af9963386a5ca1d5f3ba633539f442c3f9ed06
s121528 <s121528@163.com>
2017-12-27 09:18:53 +0800

    Java配置（@Configuration和@Bean）、依赖注入（IOC、DI）、切面（AOP）、EL

--- Learning-01-02/src/main/java/cn/hacz/edu/aop/Main.java
+++ Learning-01-02/src/main/java/cn/hacz/edu/aop/Main.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
+
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AopConfig.class);
 
         DemoAnnotationService demoAnnotationService = context.getBean(DemoAnnotationService.class);
