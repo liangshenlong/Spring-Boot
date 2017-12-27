49312c1730075dba6e45cc88777467491768b7ea
s121528 <s121528@163.com>
2017-12-27 17:46:55 +0800

    SpringBoot & RabbitMQ完成DirectExchange分布式消息消费
    SpringBoot & RabbitMQ完成DirectExchange分布式消息消费

---
+++ learning-90-01/rabbitmq-provider/src/main/java/rabbitmq/provider/user/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package rabbitmq.provider.user.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rabbitmq.provider.user.entity.UserEntity;
+
+/**
+ * 用户数据接口定义
+ * ========================
+ *
+ * @author 恒宇少年
+ * Created with IntelliJ IDEA.
+ * Date：2017/11/26
+ * Time：14:35
+ * 码云：http://git.oschina.net/jnyqy
+ * ========================
+ */
+public interface UserRepository
+        extends JpaRepository<UserEntity, Long> {
+}
