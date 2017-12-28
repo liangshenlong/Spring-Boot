192b4c4a9dbcb60c0e1c8ed3c325056744004342
s121528 <s121528@163.com>
2017-12-28 17:36:11 +0800

    Jackson2的简单使用
    Jackson2的简单使用

---
+++ Learning-11-02/src/main/java/cn/hacz/edu/learning1102/Learning1102Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu.learning1102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning1102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning1102Application.class, args);
+	}
+}
