6c948c9dbf6020f7554e0e2c5c05979d963d7ee3
s121528 <s121528@163.com>
2017-12-29 09:48:43 +0800

    XML接口调用
    XML接口调用

---
+++ Learning-13-02/src/main/java/cn/hacz/edu/Learning1302Application.java
@@ -0,0 +1,12 @@
+package cn.hacz.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learning1302Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learning1302Application.class, args);
+	}
+}
