361fdde774e587fb7e2da1ffb06f0ecdc4a30dd9
s121528 <s121528@163.com>
2018-01-02 16:16:18 +0800

    Jackson2的简单使用
    Jackson2的简单使用

---
+++ Learning-11-02/src/main/java/cn/hacz/edu/entity/User.java
@@ -0,0 +1,18 @@
+package cn.hacz.edu.entity;
+
+/**
+ * ========================
+ * project -
+ *
+ * @author guod
+ * @version 3.0
+ * @date 日期:2017/12/29 时间:9:10
+ * @JDK: v1.8
+ * @Description 功能模块：
+ * GitHub地址：https://github.com/s121528
+ * ========================
+ */
+public class User {
+
+
+}
